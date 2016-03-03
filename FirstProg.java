0bc5f1826cce7d58df918338fb674ab990448b9b
crazyshmyga <a.tiniakova@gmail.com>
2016-03-03 22:16:14 +0200

    Added files via upload

---
+++ FirstProg.java
@@ -0,0 +1,8 @@
+/**
+ * Created by A on 03.03.2016.
+ */
+public class FirstProg {
+    public static void main (String[] args){
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
