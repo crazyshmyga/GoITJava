d5a90aee94d2f2b5fbc5b841078a9c68317e4e50
crazyshmyga <a.tiniakova@gmail.com>
2016-03-10 10:15:10 +0200

    Added Module3-3

---
+++ SoundStore.java
@@ -0,0 +1,15 @@
+package com.goit.gojavaonline;
+
+import java.util.List;
+
+/**
+ * Created by A on 10.03.2016.
+ */
+public class SoundStore {
+    public static void main(String[] args){
+        List<MusicalInstrument> musicalInstruments;
+        List<Piano> pianos;
+        List<Tube> tubes;
+        List<Guitar> guitars;
+    }
+}
